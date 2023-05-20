276d7a96c6e6b68ce2946ca4d8facfd149ec9395
Aswathi P J <aswathipjsreenilayam@gmail.com>
2023-05-20 17:22:50 +0530

    add card effects

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -22,7 +22,7 @@ const opts = {
   width: '100%',
   playerVars: {
     // https://developers.google.com/youtube/player_parameters
-    autoplay: 0,
+    autoplay: 1 ,
   },
 };
 const handleMovie=(id)=>{
